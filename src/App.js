57cab9fb23fdb8b804dd79f1242cf1b9ae763519
Oluwatobi Blessing Oluyede <oluwatobioluyede4@gmail.com>
2023-10-24 09:25:44 +0100

    design dashboard

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
 
-function App() {
-  const name = "Tobi"
-  const interests = ["Gaming", "Writing", "Reading", "Music"]
+const Banner = () => {
   return (
-    <div>
-      <h1>Hello, my name is {name}</h1>
-      My interests are: 
-      <ul>
-        {
-          interests.map((interest) => {
-            return <li>{interest}</li>
-          })
-        }
-        </ul>
+    <h1 className='d-flex justify-content-center align-items-center shadow-sm p-5 bg-light text-primary'>Sextant Network Stats Dashboard</h1>
+  )
+}
+
+const Exhibit = (props) => {
+  return (
+    <div className="card" style={{width: '18rem'}}>
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
     </div>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Banner />
+      <Exhibit title="Latency"/>
+    </>
   );
 }
 
