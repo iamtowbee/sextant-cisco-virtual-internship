fb1f9c73351e12dd345ab7a3647715737566d615
Oluwatobi Blessing Oluyede <oluwatobioluyede4@gmail.com>
2023-10-24 09:57:32 +0100

    Add parent and card components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import './App.css';
 
 const Banner = () => {
   return (
-    <h1 className='d-flex justify-content-center align-items-center shadow-sm p-5 bg-light text-primary'>Sextant Network Stats Dashboard</h1>
+    <h1 className='d-flex justify-content-center align-items-center shadow-sm p-5 bg-light text-primary mb-5 '>Sextant Network Stats Dashboard</h1>
   )
 }
 
-const Exhibit = (props) => {
+const Card = (props) => {
   return (
     <div className="card" style={{width: '18rem'}}>
       <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h4 className="card-title">{props.title}</h4>
+        <p className="card-text">{props.text}</p>
       </div>
+    </div>
+  )
+}
+
+const Exhibit  = () => {
+  return (
+    <div className='d-flex gap-5 container mx-auto bg-light h-50 py-5 px-4'>
+      <Card title='Latency' text='25 ms' />
+      <Card title='Uptime' text='93%' />
+
     </div>
   )
 }
@@ -21,7 +31,7 @@ function App() {
   return (
     <>
       <Banner />
-      <Exhibit title="Latency"/>
+      <Exhibit />
     </>
   );
 }
